a74fd6a8953d784fcd19f7691cdff46a836ec40d
jovemtech <jovem@UNR04DT0065141.animaedu.intranet>
2026-06-03 16:52:41 -0300

    adicionei o arquivo loja

---
+++ Loja.py
@@ -0,0 +1,11 @@
+produtos = ['Base', 'Pó', 'Batom', 'Esmalte', 'Blush', 'Rímel', 'Paleta De Sombra']
+cliente = []
+estoquista = [ "Marlon", "Taís","Allan","Jeniffer","Aguines",]
+gerente = ["Júlio", "Paula"]
+donoEmpresa = ["Cinthia"]
+
+def estoquistas(nome_usuario):
+    """"
+    Função de acesso nível 'estoquista'.
+    Permite visualisar e mudar a lista de produtos (Adicionar, Excluir, Atualizar).
+    """
