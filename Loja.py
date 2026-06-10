0a210f164fe17b4689c802f3070ee72500cae285
jovemtech <jovem@UNR04DT0065140.animaedu.intranet>
2026-06-10 16:33:54 -0300

    modifiquei loja

--- Loja.py
+++ Loja.py
@@ -7,5 +7,20 @@ donoEmpresa = ["Cinthia"]
 def estoquistas(nome_usuario):
     """"
     Função de acesso nível 'estoquista'.
-    Permite visualisar e mudar a lista de produtos (Adicionar, Excluir, Atualizar).
+    Solicita o parâmetro 'nome_usuario' é exigido para confirmarquem está realizando o acesso.
+    Permite visualisar e mudar a lista de produtos (Adicionar, Excluir,).
     """
+    print("abrindo a página de estoquista...")
+
+    opicoesEstoquista = input("Digite o número da opção desejada:\n1 - Adicionar Produtos\n2 - Excluir Produtos\n3 - Voltar\nEscolha: ")
+    
+    if opicoesEstoquista == "1":
+        print("\nAdicionar Produtos:")
+        nome = input("Digite o produto que deseja adicionar:" )
+        produtos.append(nome)
+    elif opicoesEstoquista == "2":
+        print("\nExcluir Produtos:")
+        nome = input("Digite o produto que deseja excluir")
+        produtos.remove(nome)
+    elif opicoesEstoquista == "3":
+        print("Voltando ao menu principal...")      
